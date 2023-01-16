15a270b96cff947cebddd9f695c18ee6d44052d2
meti <mahdikhodarahimi0@gmail.com>
2023-01-16 23:45:49 +0330

    Add project files.

---
+++ Calculator/MVVM/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Dangl.Calculator;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calculator.MVVM.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class CalculatorViewModel
+    {
+        public string Formula { get; set; }
+        public double Result { get; set; } = 0;
+
+        public ICommand OperationCommand => new Command((number) =>
+        {
+            Formula += number;
+        });
+        public ICommand ResetCommand => new Command(() =>
+        {
+            Formula = "";
+            Result = 0;
+        });
+        public ICommand RemoveCommand => new Command(() =>
+        {
+            if (Formula.Length>0)
+            Formula=Formula.Remove(Formula.Length-1);
+        });
+        public ICommand CalculateCommand => new Command(() =>
+        {
+            if (Formula.Length == 0) return;
+            var calculate=Dangl.Calculator.Calculator.Calculate(Formula);
+            Result = calculate.Result;
+        });
+    }
+}
